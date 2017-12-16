3550a04d616f28b9e8d0e496fc1c895b32c7559d
bochinski <bochinski@mailbox.tu-berlin.de>
2017-12-16 12:04:11 +0100

    redo, based on smoe_test.py, still hacky

--- reg_batch_test.py
+++ reg_batch_test.py
@@ -1,62 +1,64 @@
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
-from tensorflow.python.framework import ops
-import os
-import shutil
-from time import time
 
 from smoe import Smoe
-from plotter import *
+from plotter import ImagePlotter, LossPlotter
 from utils import save_model, load_params
 
-image = plt.imread("/media/nvme/rolf/Tok1.png")
-out_base = "/media/nvme/tf_smoe/batch_test/"
-params_file = "/media/nvme/smoe/1024_120000.pkl"
 
-num = 100
-start_reg = 0.0001
-end_reg = 0.1
-regs = np.linspace(start_reg, end_reg, num)
-regs = np.flipud(regs)
+def main(image_path, results_path, iterations, kernels_per_dim, params_file):
+    orig = plt.imread(image_path)
+    if orig.dtype == np.uint8:
+        orig = orig.astype(np.float32)/255.
 
-train_iters = 300000
+    if params_file is not None:
+        init_params = load_params(params_file)
+    else:
+        init_params = None
 
+    loss_plotter = LossPlotter(path=results_path + "/loss.png", quiet=True)
+    image_plotter = ImagePlotter(path=results_path, options=['orig', 'reconstruction', 'gating', 'pis_hist'], quiet=True)
 
-if not os.path.exists(out_base):
-    os.mkdir(out_base)
-else:
-    shutil.rmtree(out_base + "/")
-    os.mkdir(out_base)
-
-
-for idx, reg in enumerate(list(regs)):
-    ops.reset_default_graph()
-
-    #lr_scale = start_reg/reg
-
-    out_dir = out_base + '/{0:.8f}'.format(reg)
-    os.mkdir(out_dir)
-
-    loss_plotter = LossPlotter(path=out_dir+"/loss.png", quiet=True)
-    image_plotter = ImagePlotter(path=out_dir, options=['orig', 'reconstruction', 'gating', 'pis_hist'], quiet=True)
-
-    optimizer1 = tf.train.AdamOptimizer(0.001)
-    optimizer2 = tf.train.AdamOptimizer(0.000005)
+    smoe = Smoe(orig, kernels_per_dim, init_params=init_params, pis_relu=True, train_pis=True, pis_l1=0.05)
 
     #optimizer1 = tf.train.AdamOptimizer(0.005, beta1=0.05, beta2=0.1, epsilon=0.1)
-    #optimizer1 = tf.train.GradientDescentOptimizer(0.001 * lr_scale)
-    #optimizer2 = tf.train.GradientDescentOptimizer(0.0001 * lr_scale)
-    #optimizer1 = tf.train.GradientDescentOptimizer(0.001)
     #optimizer2 = tf.train.GradientDescentOptimizer(0.0001)
+    optimizer1 = tf.train.AdamOptimizer(0.0001)
+    optimizer2 = tf.train.AdamOptimizer(0.000001)
+    # optimizer1 = tf.train.GradientDescentOptimizer(0.000001)
+    # optimizer2 = tf.train.GradientDescentOptimizer(0.00000001)
 
-    #params = load_params(params_file)
+    smoe.train(iterations, optimizer1=optimizer1, optimizer2=optimizer2,
+               callbacks=[loss_plotter.plot, image_plotter.plot])
 
-    smoe = Smoe(image=image, kernels_per_dim=32, train_pis=True, pis_l1=reg, pis_relu=True)
-    start = time()
-    smoe.train(train_iters, optimizer1=optimizer1, optimizer2=optimizer2, callbacks=[loss_plotter.plot, image_plotter.plot])
-    end = time()
-    print('[{0:.0f}/{1:.0f}] reg={2:.8f}, time={3:.2f}s'.format(idx, num, reg, end - start))
+    num = 200
+    start_reg = 0.000001
+    end_reg = 0.03
+    regs = np.linspace(start_reg, end_reg, num)
 
-    save_model(smoe, out_dir + "/params_best.pkl", best=True)
-    save_model(smoe, out_dir + "/params_last.pkl", best=False)
+    #regs = np.flipud(0.03/np.linspace(1, 30000, 200))
 
+    for reg in list(regs):
+        out_path = results_path + '/{0:.8f}'.format(reg)
+        loss_plotter = LossPlotter(path=out_path + "/loss.png", quiet=True)
+        image_plotter = ImagePlotter(path=out_path, options=['orig', 'reconstruction', 'gating', 'pis_hist'],
+                                     quiet=True)
 
+        smoe.train(iterations, pis_l1=reg, callbacks=[loss_plotter.plot, image_plotter.plot])
+
+        save_model(smoe, out_path + "/params_best.pkl", best=True)
+        save_model(smoe, out_path + "/params_last.pkl", best=False)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--image', type=str, required=True, help="input image")
+    parser.add_argument('-r', '--results', type=str, required=True, help="results path")
+    parser.add_argument('-n', '--iterations', type=int, default=10000, help="number of iterations")
+    parser.add_argument('-k', '--kernels', type=int, default=12, help="number of kernels per dimension")
+    parser.add_argument('-p', '--params', type=str, default=None, help="parameter file for model initialization")
+
+    args = parser.parse_args()
+
+    main(args.image, args.results, args.iterations, args.kernels, args.params)
